b2c83469311674c431a68ff66d1b97e595c4288d
LeopoldoIII <nathan.rx@gmail.com>
2022-05-27 17:45:17 -0500

    forms and strong params

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'courses', to: 'courses#index'
-  get 'courses/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'courses', to: 'courses#index'
+  get 'courses/new'
+
+  post 'courses', to: 'courses#create'
+
 end
